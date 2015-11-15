dd3f8ce8d9378c4ba539d320b5bf4d887ec3c57a
Anna <aniawujek@gmail.com>
2015-11-15 19:07:39 +0100

    dziala znajdowanie wzmocnienia i stalej czasowej

--- zadA.m
+++ zadA.m
@@ -1,6 +1,6 @@
 close all;
 
-global alfa1 beta Umin alfa2 us g poprz_h skok t_skoku Y_WYJSCIE U
+global alfa1 beta Umin alfa2 us g poprz_h skok t_skoku U
 
 %init
 skok = 0;
@@ -25,54 +25,53 @@ if us < 1.7
     us = 0;
 end
 
-h0 = [0; 0];
+h0_18 = [0; 0];
 poprz_h = [0;0];
 OPTIONS = odeset('MaxStep', 50);
-[T_WYJSCIE, H_WYJSCIE] = ode45(@obiektReg, [0 10000], h0, OPTIONS); 
-pom1 = 0.068*H_WYJSCIE(:,1)+ones(size(H_WYJSCIE)(1),1)*0.5116;
-pom2 = 0.0883*H_WYJSCIE(:,2)+ones(size(H_WYJSCIE)(1),1)*0.6066;
-Y_WYJSCIE = [T_WYJSCIE, pom1, pom2];
+[T_WYJSCIE_18, H_WYJSCIE_18] = ode45(@obiektReg, [0 10000], h0_18, OPTIONS); 
+y1_18 = 0.068*H_WYJSCIE_18(:,1)+ones(size(H_WYJSCIE_18)(1),1)*0.5116;
+y2_18 = 0.0883*H_WYJSCIE_18(:,2)+ones(size(H_WYJSCIE_18)(1),1)*0.6066;
+Y_WYJSCIE_18 = [T_WYJSCIE_18, y1_18, y2_18];
 
 %obliczenie wzmocnienia i stalej czasowej
-h_ustalone = [0 0];
-for i = 10:size(H_WYJSCIE)(1)-1
-    if abs(H_WYJSCIE(i,1)-H_WYJSCIE(i+1,1))<0.01 && abs(H_WYJSCIE(i,2)-H_WYJSCIE(i+1,2))<0.01
-        h_ustalone = [H_WYJSCIE(i,:)]
+h_ustalone_18 = [0 0];
+for i = 10:size(H_WYJSCIE_18)(1)-1
+    if abs(H_WYJSCIE_18(i,1)-H_WYJSCIE_18(i+1,1))<0.0001 && abs(H_WYJSCIE_18(i,2)-H_WYJSCIE_18(i+1,2))<0.0001
+        h_ustalone_18 = [H_WYJSCIE_18(i,:)];
         break;
     end
 end
 
-K = [0 0];
-K = h_ustalone/us;
-K
-for i = 10:size(H_WYJSCIE)(1)
-    if H_WYJSCIE(i,1)-0.63*h_ustalone(1)<0 && H_WYJSCIE(i,2)-0.63*h_ustalone(2)>=0
-        T1 = T_WYJSCIE(i);
-    end
-    if H_WYJSCIE(i,1)-0.95*h_ustalone(1)<0 && H_WYJSCIE(i,2)-0.95*h_ustalone(2)>=0
-        T3 = T_WYJSCIE(i);
-    end
-    if H_WYJSCIE(i,1)-0.99*h_ustalone(1)<0 && H_WYJSCIE(i,2)-0.99*h_ustalone(2)>=0
-        T5 = T_WYJSCIE(i);
+K_18 = [0 0];
+K_18 = h_ustalone_18/us;
+K_18
+T_18 = [0 0];
+for i = 10:size(H_WYJSCIE_18)(1)-1
+    if H_WYJSCIE_18(i,1)-0.63*h_ustalone_18(1)<0 && H_WYJSCIE_18(i+1,1)-0.63*h_ustalone_18(1)>=0
+        T_18(1) = T_WYJSCIE_18(i);
     end
 end
-T1
-T3
-T5
+
+for i = 10:size(H_WYJSCIE_18)(1)-1
+    if real(H_WYJSCIE_18(i,2)-0.63*h_ustalone_18(2))<0 && real(H_WYJSCIE_18(i+1,2)-0.63*h_ustalone_18(2))>=0
+        T_18(2) = T_WYJSCIE_18(i);
+    end
+end
+T_18
 
 %koniec obliczania wzmocnienia i stalej czasowej
 
 figure;
 
 subplot(2,2,1);
-plot(T_WYJSCIE,H_WYJSCIE(:,1));
+plot(T_WYJSCIE_18,H_WYJSCIE_18(:,1));
 hold on;
-t_lin = linspace(0,10000,10000);
-h1_lin1 = K(1)*1.8*(1-e.^(-t_lin/T1));
-plot(t_lin,h1_lin1,'r');
+t_lin_10000 = linspace(0,10000,10000);
+h1_lin_18 = K_18(1)*us*(1-e.^(-t_lin_10000/T_18(1)));
+plot(t_lin_10000,h1_lin_18,'r');
 U = sortrows(U);
 plot(U(:,1),U(:,2),'g');
-plot([t_skoku t_skoku],[0,20],'m');
+plot([t_skoku t_skoku],[0,30],'m');
 legend('H1','model','us','zalaczenie');
 grid on;
 title('H1, u=1.8');
@@ -81,66 +80,125 @@ ylabel('H1');
 
 
 subplot(2,2,2);
-plot(T_WYJSCIE,H_WYJSCIE(:,2));
+plot(T_WYJSCIE_18,H_WYJSCIE_18(:,2));
+hold on;
+h2_lin_18 = K_18(2)*us*(1-e.^(-t_lin_10000/T_18(2)));
+plot(t_lin_10000,h2_lin_18,'r');
+U = sortrows(U);
+plot(U(:,1),U(:,2),'g');
+plot([t_skoku t_skoku],[0,20],'m');
+legend('H2','model','us','zalaczenie');
 grid on;
 title('H2, u=1.8');
 xlabel('t');
 ylabel('H2');
 
 subplot(2,2,3);
-plot(Y_WYJSCIE(:,1),Y_WYJSCIE(:,2));
+plot(Y_WYJSCIE_18(:,1),Y_WYJSCIE_18(:,2));
 grid on;
 title('Y1, u=1.8');
 xlabel('t');
 ylabel('Y1');
 
 subplot(2,2,4);
-plot(Y_WYJSCIE(:,1),Y_WYJSCIE(:,3));
+plot(Y_WYJSCIE_18(:,1),Y_WYJSCIE_18(:,3));
 grid on;
 title('Y2, u=1.8');
 xlabel('t');
 ylabel('Y2');
 
+
+% *************************************** %
+%                  us = 4                 %
+% *************************************** %
+
 %init
-Y_WYJSCIE = [];
-h_hist=[];
-h0 = [0; 0];
-poprz_h = [0;0];
 skok = 0;
+t_skoku = 0;
+U = [0 0];
 us = 4;
 %end init
 
-OPTIONS = odeset('MaxStep', 50,'NonNegative',1);
-[TT_WYJSCIE, YH_WYJSCIE] = ode45(@obiektReg, [0 20000], h0, OPTIONS); 
-pom1 = 0.068*YH_WYJSCIE(:,1)+ones(size(YH_WYJSCIE)(1),1)*0.5116;
-pom2 = 0.0883*YH_WYJSCIE(:,2)+ones(size(YH_WYJSCIE)(1),1)*0.6066;
-Y_WYJSCIE = [TT_WYJSCIE, pom1, pom2];
+
+h0_4 = [0; 0];
+poprz_h = [0;0];
+OPTIONS = odeset('MaxStep', 50);
+[T_WYJSCIE_4, H_WYJSCIE_4] = ode45(@obiektReg, [0 20000], h0_4, OPTIONS); 
+y1_4 = 0.068*H_WYJSCIE_4(:,1)+ones(size(H_WYJSCIE_4)(1),1)*0.5116;
+y2_4 = 0.0883*H_WYJSCIE_4(:,2)+ones(size(H_WYJSCIE_4)(1),1)*0.6066;
+Y_WYJSCIE_4 = [T_WYJSCIE_4, y1_4, y2_4];
+
+%obliczenie wzmocnienia i stalej czasowej
+h_ustalone_4 = [0 0];
+for i = 10:size(H_WYJSCIE_4)(1)-1
+    if abs(H_WYJSCIE_4(i,1)-H_WYJSCIE_4(i+1,1))<0.0001 && abs(H_WYJSCIE_4(i,2)-H_WYJSCIE_4(i+1,2))<0.0001
+        h_ustalone_4 = [H_WYJSCIE_4(i,:)];
+        break;
+    end
+end
+
+K_4 = [0 0];
+K_4 = h_ustalone_4/us;
+K_4
+T_4 = [0 0];
+for i = 10:size(H_WYJSCIE_4)(1)-1
+    if H_WYJSCIE_4(i,1)-0.63*h_ustalone_4(1)<0 && H_WYJSCIE_4(i+1,1)-0.63*h_ustalone_4(1)>=0
+        T_4(1) = T_WYJSCIE_4(i);
+    end
+end
+
+for i = 10:size(H_WYJSCIE_4)(1)-1
+    if real(H_WYJSCIE_4(i,2)-0.63*h_ustalone_4(2))<0 && real(H_WYJSCIE_4(i+1,2)-0.63*h_ustalone_4(2))>=0
+        T_4(2) = T_WYJSCIE_4(i);
+    end
+end
+T_4
+
+%koniec obliczania wzmocnienia i stalej czasowej
 
 figure;
+
 subplot(2,2,1);
-plot(TT_WYJSCIE,YH_WYJSCIE(:,1));
+plot(T_WYJSCIE_4,H_WYJSCIE_4(:,1));
+hold on;
+t_lin_20000 = linspace(0,20000,20000);
+h1_lin_4 = K_4(1)*us*(1-e.^(-t_lin_20000/T_4(1)));
+plot(t_lin_20000,h1_lin_4,'r');
+U = sortrows(U);
+plot(U(:,1),U(:,2),'g');
+plot([t_skoku t_skoku],[0,300],'m');
+legend('H1','model','us','zalaczenie');
 grid on;
-title('H1, u=4');
+title('H1, u=1.8');
 xlabel('t');
 ylabel('H1');
 
+
 subplot(2,2,2);
-plot(TT_WYJSCIE,YH_WYJSCIE(:,2));
+plot(T_WYJSCIE_4,H_WYJSCIE_4(:,2));
+hold on;
+h2_lin_4 = K_4(2)*us*(1-e.^(-t_lin_20000/T_4(2)));
+plot(t_lin_20000,h2_lin_4,'r');
+U = sortrows(U);
+plot(U(:,1),U(:,2),'g');
+plot([t_skoku t_skoku],[0,150],'m');
+legend('H2','model','us','zalaczenie');
 grid on;
-title('H2, u=4');
+title('H2, u=1.8');
 xlabel('t');
 ylabel('H2');
 
 subplot(2,2,3);
-plot(Y_WYJSCIE(:,1),Y_WYJSCIE(:,2));
+plot(Y_WYJSCIE_4(:,1),Y_WYJSCIE_4(:,2));
 grid on;
-title('Y1, u=4');
+title('Y1, u=1.8');
 xlabel('t');
 ylabel('Y1');
 
 subplot(2,2,4);
-plot(Y_WYJSCIE(:,1),Y_WYJSCIE(:,3));
+plot(Y_WYJSCIE_4(:,1),Y_WYJSCIE_4(:,3));
 grid on;
-title('Y2, u=4');
+title('Y2, u=1.8');
 xlabel('t');
 ylabel('Y2');
+
